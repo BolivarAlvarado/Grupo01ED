3d2763d0f5bf28c8b66904406616200bdbd6c8d1
MichelleArreaga <107522566+MichelleArreaga@users.noreply.github.com>
2025-08-07 18:27:28 -0500

    Implementacion TDA List
    Clase abstracta con los metodos a implementar de las listas creadas

--- TicTacToe/src/tda/List.java
+++ TicTacToe/src/tda/List.java
@@ -8,6 +8,18 @@ package tda;
  *
  * @author Michelle
  */
-public interface List {
-    
+public interface List<E> {
+ boolean isEmpty();
+    int size();
+    boolean addFirst(E element);
+    boolean addlast(E element);
+    boolean removeFirst();
+    boolean removeLast();
+    boolean remove(E element);
+    E remove(int index); // retorna el elemento removido
+    E set(int index,E element); //cambia el elemento en la posicion de index y retorna el elemento reemplazado
+    E getFirst();
+    E getLast();
+    boolean insert(int index, E element);
+    boolean contains(E element);   
 }
