cf04daef6082daed2a06f830c965c2635f4261c2
MichelleArreaga <107522566+MichelleArreaga@users.noreply.github.com>
2025-08-08 05:08:10 -0500

    Creacion de interfaz grafica principal y ajustes
    Las ventanas para el menu del juego y los ajustes de quien empieza y que simbolo tiene cada jugador estan creadas, mas no implementadas

---
+++ TicTacToe/src/main/controlador/AjustesController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package main.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+/**
+ * FXML Controller class
+ *
+ * @author Michelle
+ */
+public class AjustesController implements Initializable {
+
+    @FXML
+    private RadioButton btnX;
+    @FXML
+    private ToggleGroup tgSimbolo;
+    @FXML
+    private RadioButton btnO;
+    @FXML
+    private RadioButton btnJugador1;
+    @FXML
+    private ToggleGroup tgInicio;
+    @FXML
+    private RadioButton btnJugador2;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
