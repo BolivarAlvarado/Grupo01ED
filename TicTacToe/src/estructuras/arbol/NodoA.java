1eee19c0e1f573900b2263ddb89175c61e64e0b3
MichelleArreaga <107522566+MichelleArreaga@users.noreply.github.com>
2025-08-08 00:32:31 -0500

    Comentarios de la clase Nodo del arbol
    Comentarios a los metodos y variables correspondientes a la clase Nodo del arbol

--- TicTacToe/src/estructuras/arbol/NodoA.java
+++ TicTacToe/src/estructuras/arbol/NodoA.java
@@ -7,35 +7,69 @@ package estructuras.arbol;
 import estructuras.listas.LinkedList;
 
 /**
+ * Clase que representa un nodo en un árbol, donde cada nodo puede tener varios
+ * hijos.
  *
+ * @param <E> Tipo del dato almacenado en el nodo.
  * @author Michelle
  */
 class NodoA<E> {
-    private E dato;
-    private LinkedList<NodoA<E>> hijos;
-    
+
+    private E dato;  // Dato almacenado en el nodo
+    private LinkedList<NodoA<E>> hijos;  // Lista de hijos del nodo
+
+    /**
+     * Constructor para crear un nodo con un dato específico.
+     *
+     * @param dato Dato que se almacenará en el nodo.
+     */
     public NodoA(E dato) {
         this.dato = dato;
-        this.hijos = new LinkedList<>();
+        this.hijos = new LinkedList<>();  // Inicializa la lista de hijos como vacía
     }
 
+    /**
+     * Obtiene el dato almacenado en el nodo.
+     *
+     * @return El dato almacenado en el nodo.
+     */
     public E getDato() {
         return dato;
     }
 
+    /**
+     * Establece un nuevo valor para el dato almacenado en el nodo.
+     *
+     * @param dato Nuevo dato que se almacenará en el nodo.
+     */
     public void setDato(E dato) {
         this.dato = dato;
     }
 
+    /**
+     * Obtiene la lista de hijos del nodo.
+     *
+     * @return Lista de hijos del nodo.
+     */
     public LinkedList<NodoA<E>> getHijos() {
         return hijos;
     }
 
+    /**
+     * Agrega un hijo al nodo.
+     *
+     * @param hijo El nodo hijo que se agregará.
+     */
     public void agregarHijo(NodoA<E> hijo) {
-        hijos.addLast(hijo);
+        hijos.addLast(hijo);  // Agrega el hijo al final de la lista de hijos
     }
 
+    /**
+     * Verifica si el nodo tiene hijos.
+     *
+     * @return true si el nodo tiene al menos un hijo, false en caso contrario.
+     */
     public boolean tieneHijos() {
-        return !hijos.isEmpty();
+        return !hijos.isEmpty();  // Devuelve true si la lista de hijos no está vacía
     }
 }
