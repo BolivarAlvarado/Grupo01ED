d8a8858bb1035b7fa047c29ed102ebd950e59eff
MichelleArreaga <107522566+MichelleArreaga@users.noreply.github.com>
2025-08-07 19:01:46 -0500

    Comentarios para clase Nodo de listas
    Se agregan comentarios sobre los paametros y funcionalidad de la clase Nodo para las listas enlazadas

--- TicTacToe/src/estructuras/listas/Nodo.java
+++ TicTacToe/src/estructuras/listas/Nodo.java
@@ -10,28 +10,50 @@ package estructuras.listas;
  * @param <E> Tipo de dato que almacena el nodo.
  */
 public class Nodo<E> {
-
+    
+    // Valor almacenado en el nodo
     private E data;
     
+    // Enlace al siguiente nodo
     private Nodo<E> next;
 
+    /**
+     * Constructor que recibe el dato a almacenar.
+     * @param data Elemento que guarda el nodo
+     */
     public Nodo(E data) {
         this.data = data;
         this.next = null;
     }
 
+    /**
+     * Establece el dato del nodo.
+     * @param data Nuevo valor a guardar
+     */
     public void setData(E data) {
         this.data = data;
     }
 
+    /**
+     * Establece la referencia al siguiente nodo.
+     * @param next Nodo siguiente en la lista
+     */
     public void setNext(Nodo<E> next) {
         this.next = next;
     }
 
+    /**
+     * Obtiene el dato almacenado.
+     * @return Elemento guardado en el nodo
+     */
     public E getData() {
         return data;
     }
 
+    /**
+     * Obtiene el siguiente nodo enlazado.
+     * @return Nodo siguiente
+     */
     public Nodo<E> getNext() {
         return next;
     }
