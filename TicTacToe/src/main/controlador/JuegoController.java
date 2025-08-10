93af0ee794539e94a7d73fd02cd2900057bea25d
MichelleArreaga <107522566+MichelleArreaga@users.noreply.github.com>
2025-08-10 06:04:27 -0500

    Jugar sin validar ganador
    La pantalla de juego permite jugar, sin embargo aun no verifica el ganador, implementacion no completa

--- TicTacToe/src/main/controlador/JuegoController.java
+++ TicTacToe/src/main/controlador/JuegoController.java
@@ -34,12 +34,11 @@ public class JuegoController implements Initializable {
     @FXML
     private GridPane gridTablero;
 
-    private Button[][] btnCeldas;
-
     private ConfiguracionJuego config;
     private Jugador j1;
     private Jugador j2;
     private Tablero tablero;
+    private Button[][] btnCeldas;
 
     /**
      * Initializes the controller class.
@@ -62,51 +61,67 @@ public class JuegoController implements Initializable {
     @FXML
     private void regresar(ActionEvent event) {
         abrirVentana("Ajustes", event);
-    }
-
-    private Button[][] botones;
+    }    
 
     private void configTablero() {
-        botones = new Button[Tablero.SIZE][Tablero.SIZE];
+        btnCeldas = new Button[Tablero.SIZE][Tablero.SIZE];
         for (int fila = 0; fila < Tablero.SIZE; fila++) {
             for (int col = 0; col < Tablero.SIZE; col++) {
                 Button celda = new Button();
-                botones[fila][col] = celda;
+                celda.getStyleClass().add("cell-button");
+                btnCeldas[fila][col] = celda;
                 int f = fila, c = col;
                 gridTablero.add(celda, col, fila);
-                celda.setOnAction(e -> manejarClickCelda(f, c));
+                celda.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+                celda.setOnAction(e -> jugar(f, c));
             }
         }
     }
 
     private Button obtenerBotonPorCoordenadas(int fila, int columna) {
-        return botones[fila][columna];
+        return btnCeldas[fila][columna];
     }
 
-    private void manejarClickCelda(int fila, int columna) {
+    private void jugar(int fila, int columna) {
         Button boton = obtenerBotonPorCoordenadas(fila, columna);
-        if (boton.getText().isEmpty()) {
-            if (j1.isJugando()) {
-                boton.setText("X");
+
+        if (boton.getGraphic() == null) { 
+            String imagen;
+            if (j1.isJugando()) { 
+                tablero.colocarMarca(fila, columna, j1.getCaracter());
+                imagen = "recursos/img/" + j1.getCaracter() + ".png";
                 cambiarTurno();
             } else {
-                boton.setText("O");
+                tablero.colocarMarca(fila, columna, j2.getCaracter());
+                imagen = "recursos/img/" + j2.getCaracter() + ".png";
                 cambiarTurno();
             }
+
+            boton.setStyle(
+                    "-fx-background-image: url('" + imagen + "');"
+                    + "-fx-background-size: cover;"
+                    + "-fx-background-position: center;"
+                    + "-fx-background-repeat: no-repeat;"
+                    + "-fx-background-size: 80% 80%;"
+            );
+            
             boton.setDisable(true);
         }
     }
 
     private void cambiarTurno() {
-        if (j1.isJugando()) {
-            j1.setJugando(false);
-            j2.setJugando(true);
-            actualizarLablelJugador();
-        } else {
-            j2.setJugando(false);
-            j1.setJugando(true);
-            actualizarLablelJugador();
+        if (!tablero.estaLleno()) {
+            if (j1.isJugando()) {
+                j1.setJugando(false);
+                j2.setJugando(true);
+                actualizarLablelJugador();
+            } else {
+                j2.setJugando(false);
+                j1.setJugando(true);
+                actualizarLablelJugador();
+            }
         }
+
     }
 
     private void actualizarLablelJugador() {
